0e1c81724d58960f11b70ae89dc284344186a4e5
Axi404 <axihelloworld@gmail.com>
2024-08-22 02:35:30 +0800

    add url

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ import { defineSiteConfig } from 'valaxy'
 export default defineSiteConfig({
   lang: 'zh-CN',
   title: 'Axi\'s Blog',
-  url: '',
+  url: 'https://axi404.github.io/',
   author: {
     avatar: 'https://axi404.github.io/Blog/img/avatar_hu12164355173948498683.png',
     name: '阿汐',
