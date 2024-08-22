1471e91b89c6459e58a11f832b91d03055961a77
Axi404 <axihelloworld@gmail.com>
2024-08-22 20:50:21 +0800

    update waline

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -65,7 +65,7 @@ export default defineConfig<ThemeConfig>({
   addons: [
     addonWaline({
       comment: true,
-      serverURL: 'https://waline-t-iota.vercel.app/',
+      serverURL: 'https://waline-ez0elemii-axi404s-projects.vercel.app/',
       emoji: [
         '//unpkg.com/@waline/emojis@1.0.1/weibo',
         '//unpkg.com/@waline/emojis@1.0.1/bilibili',
