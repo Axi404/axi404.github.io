dc5b008f556be785342333ea8ae1f3f69243892a
Axi404 <Axi404@users.noreply.github.com>
2024-09-21 01:43:26 +0000

    deploy: 2c0bfd072e48535df352353e8cff07106d470243

---
+++ assets/categories-b1QjtiV6.js
@@ -0,0 +1,1 @@
+import{_ as r,v as i,b as m,e as p,w as o,j as n,aP as f,aQ as l,aR as u,aS as d,aT as y}from"./app-JCmIxXXV.js";const H={};function v(w,x){const e=f,a=l,t=i("router-view"),_=u,c=d,s=y;return m(),p(s,null,{default:o(()=>[n(e),n(a,{title:"分类"}),n(_,null,{default:o(()=>[n(t)]),_:1}),n(c)]),_:1})}const B=r(H,[["render",v]]);export{B as default};
